45964614214357b84c5e2739d90be5007f9311d5
mhussainahmad <mhussainahmad.me19asmme@student.nust.edu.pk>
2022-07-15 12:53:40 +0500

    add get_data

--- src/get_data.py
+++ src/get_data.py
@@ -1,6 +1,6 @@
-##Read params
-## process
-## return dataframe
+# Read params
+# process
+# return dataframe
 
 import os
 import yaml
@@ -16,12 +16,14 @@ def read_params(config_path):
 
 def get_data(config_path):
     config = read_params(config_path)
-    print(config)
-    ##data_path = config[]
+    # print(config)
+    data_path = config["data_source"]["s3_source"]
+    df = pd.read_csv(data_path, sep=",", encoding='utf-8')
+    return df
 
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config", default='params.yaml')
+    args.add_argument("--config", default="../params.yaml")
     parsed_args = args.parse_args()
-    get_data(parsed_args.config)
+    data = get_data(parsed_args.config)
