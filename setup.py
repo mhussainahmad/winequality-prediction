d45b87637ec3bdb7a7d849641b01b45a16d02430
mhussainahmad <mhussainahmad.me19asmme@student.nust.edu.pk>
2022-07-19 04:20:38 +0500

    web structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="It's a wine quality prediction project",
+    author="hussain",
+    packages=find_packages(),
+    license="MIT"
+)
